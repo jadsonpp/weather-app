969cbf736fb66434b195622138052f01730b0049
jadsonpp <jadson_pereira5@hotmail.com>
2021-02-05 02:00:27 -0300

    Att: Search History

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home,search
+from .views import home, search, search_history
 
 urlpatterns = [
     path('',home, name="index"),
-    path('place/',search,name="search")
+    path('place/',search,name="search"),
+    path('history/',search_history,name="search_history"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,7 @@
-#   coding: utf-8
 from django.shortcuts import render
 import requests
-from .extras import (
-    Weather,
-    weatherInfos
-)  
-
+from .extras import weatherInfos
+from .models import Search
 from .forms import CityForm
 
 params = {
@@ -21,19 +17,36 @@ def home(request):
 def search(request):
     
     if request.method=="POST":
-
+        # get the location of interesting.
         cityName = request.POST['City']
+        # change the dictionary {access_key, param's}
         params['query'] = cityName
-    
+
+        # Search on API
         api_result = requests.get('http://api.weatherstack.com/current', params)
         api_response = api_result.json()
         
+        # If the location doesn't exist show up a error page.
         if ('error' in api_response):
             msg = "Error, Place data not found."
             return render(request,'error.html', {'form': msg})
-
+        
         weather = weatherInfos(api_response)
-    
+        # Saving the searchs
+        search = Search()
+        search.fillData(location=weather.name, temperature=weather.temperature, 
+                        description=weather.description)
+        search.save()
+
         return render(request,'weather.html',{'form': weather})
     
     return render(request,'base.html')
+
+
+def search_history(request):
+    searches = Search.objects.all()
+    
+    for obj in searches:
+        obj.showData()
+        
+    return render(request, 'history.html', {'searches': searches} )
